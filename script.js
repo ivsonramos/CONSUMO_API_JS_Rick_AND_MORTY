d6fb48b1f1e05e4ed9439f754c6bcc867658e8d9
ivsonramos <ivson.ramos@gmail.com>
2024-10-10 21:18:23 -0300

    :construction:Ajusta ID do input

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ const content = document.getElementById('content');
 const image = document.getElementById('img');
 
 const fetchApi = (value) => {
-    const result = fetch(`https://rickandmortyapi.com/api/character/${value}`).then((res) => res.json()).then((data) => {console.log(data);
+    const result = fetch(`https://rickandmortyapi.com/api/character/${value}`)
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data);
         return data;
     });
    return result;
